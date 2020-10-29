5272336b28685ec802323a3bd8bad81b008dfc89
Juan Pablo Salazar Vásquez <66329559+JuanPabloSalazarVasquez@users.noreply.github.com>
2020-10-29 16:36:14 -0500

    Arreglado el tablero, clores e imágenes PNG

--- Prototipo/src/Components/Calendario.js
+++ Prototipo/src/Components/Calendario.js
@@ -1,25 +1,71 @@
-import React from "react";
-import "../Styles/Calendario.css";
+import React, { Fragment } from 'react';
+import useCalendar from '../hooks/useCalendar';
+import { Link, withRouter} from "react-router-dom";
 
+import "../Styles/Calendario.css";
 import "../Global.css";
 
-export default function Calendario() {
-  return (
-    <div className="Calendario">
-      <div className="separador"></div>
+const Calendario = (props) => {
+const {history} = props
+  const { calendarRows, selectedDate, todayFormatted, daysShort, monthNames, getNextMonth, getPrevMonth } = useCalendar();
 
-      <div className="contenedor">
-        <div className="calendario_img center_txt">
-          <img
-            src="./img/calendario.png"
-            alt="Calendario"
-          />
-        </div>
+  const dateClickHandler = date => {
+    console.log(date);
+  }
 
-        <div className="center_txt">
-          <button> OK </button>
-        </div>
-      </div>
+  return(
+    <Fragment>
+      <p>Selected Month: {`${monthNames[selectedDate.getMonth()]} - ${selectedDate.getFullYear()}`}</p>
+      <table className="table">
+        <thead>
+          <tr>
+            {daysShort.map(day => (
+              <th key={day}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Object.values(calendarRows).map(cols => {
+              return <tr key={cols[0].date}>
+                {cols.map(col => (
+                  col.date === todayFormatted
+                    ? <td key={col.date} className={`${col.classes} today`} onClick={() => dateClickHandler(col.date)}>
+                      {col.value}
+                    </td>
+                    : <td key={col.date} className={col.classes} onClick={() => dateClickHandler(col.date)}>{col.value}</td>
+                ))}
+              </tr>
+            })
+          }
+        </tbody>
+      </table>
+
+      <button className="button" onClick={getPrevMonth}>Prev</button>
+      <button className="button" onClick={getNextMonth}>Next</button>
+    
+      <div class="container">
+  <h2>Stacked form</h2>
+  <form action="/Carrusel">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"/>
     </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"/> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+    
+    </Fragment>
   );
 }
+
+export default withRouter(Calendario);
