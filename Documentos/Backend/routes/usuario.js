73aa72065cedd8aa93fda5a1c65539a745f38e7a
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2020-11-11 20:04:38 +0000

    Estructura del backend

---
+++ Documentos/Backend/routes/usuario.js
@@ -0,0 +1,68 @@
+const { Router } = require('express');
+const router = Router();
+
+const mysqlConnection = require('../db/db.js');
+
+router.get('/', (req, res) => {
+  res.send('Si funciona')
+})
+
+// Usuario
+//Petición get
+router.get('/usuario', (req, res) => {
+  mysqlConnection.query('SELECT * FROM usuario',
+    (err, rows, fields) => {
+      if (!err) {
+        res.json(rows);
+      } else {
+        console.log(err);
+      }
+    })
+})
+
+//Petición post
+router.post('/usuario', (req, res) => {
+  const { id_usuario, nombre_usuario, email, contraseña, id_tipo_usuario } = req.body
+  let tipo = [id_usuario, nombre_usuario, email, contraseña, id_tipo_usuario];
+  let nuevoTipo = `INSERT INTO usuario VALUES (?,?,?,?,?);`
+
+  mysqlConnection.query(nuevoTipo, tipo, (err, results, fields) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ message: `¡Usuario nuevo registrado con exito!` })
+  });
+});
+
+//Petición put
+router.put('/usuario/:id', (req, res) => {
+  const { email, contraseña, id_tipo_usuario } = req.body
+  const { id } = req.params
+
+  mysqlConnection.query(`UPDATE usuario
+                       SET email = ?,
+                       contraseña = ?,
+                       id_tipo_usuario = ?
+                       WHERE nombre_usuario = ?`,
+    [email, contraseña, id_tipo_usuario, id], (err, rows, fields) => {
+      if (!err) {
+        res.json({ status: `Tipo usuario ha sido actualizado con éxito` });
+      } else {
+        console.log(err);
+      }
+    });
+});
+
+//PETICIÓN O SERVICIO DELETE - ELIMINACIÓN DE DATOS
+router.delete('/usuario/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query(`DELETE FROM usuario WHERE nombre_usuario =?`, [id], (err, rows, fields) => {
+    if ("!err") {
+      res.json({ status: `El usuario ha sido eliminado con exito` })
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+module.exports = router;
